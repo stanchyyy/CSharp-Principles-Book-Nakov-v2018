de425c43010e73b02c951624c1b58302c7dc2402
stanchyyy <stanchyyy@gmail.com>
2021-09-20 02:19:34 +0300

    -initial project creation;

---
+++ IntroToProgramming/IntroToProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroToProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

a7532513b862c331f81f182173fb7b818aa20a40
stanchyyy <stanchyyy@gmail.com>
2021-09-22 01:05:50 +0300

    solved home work 7 - pring 1,101,1001;

---
+++ 6.HW_Print11011001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.HW_Print11011001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberList = new List<int> { 1, 101, 1001 };
+            foreach (int number in numberList)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}

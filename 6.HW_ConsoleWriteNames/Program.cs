9e41a9f98f923fd832eb90dbb60e712d41489c87
stanchyyy <stanchyyy@gmail.com>
2021-09-22 01:01:10 +0300

    solved HW 6.

---
+++ 6.HW_ConsoleWriteNames/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _6.HW_ConsoleWriteNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Stanislav";
+            string lastName = "Atanasov";
+
+            Console.WriteLine($"My name is {firstName} {lastName}.");
+        }
+    }
+}

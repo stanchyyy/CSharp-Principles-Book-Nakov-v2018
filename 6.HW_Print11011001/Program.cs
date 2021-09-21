efc8f7f6238ebe255b3f5ff556790d701896740e
stanchyyy <stanchyyy@gmail.com>
2021-09-22 01:39:30 +0300

    7hw converted from static list to dynamic. the numbers can go on frever 8hw added

--- 6.HW_Print11011001/Program.cs
+++ 6.HW_Print11011001/Program.cs
@@ -7,11 +7,19 @@ namespace _6.HW_Print11011001
     {
         static void Main(string[] args)
         {
-            List<int> numberList = new List<int> { 1, 101, 1001 };
-            foreach (int number in numberList)
+            //Task 7: Write program that prints the numbers 1,101,1001 each on new line.
+            int maxNumber = 1001;
+            int multiplyer = 10;
+            int one = 1;
+            Console.WriteLine(one);
+            do
             {
-                Console.WriteLine(number);
+
+                multiplyer *= 10;
+                Console.WriteLine(one + multiplyer);
             }
+            while (one + multiplyer < maxNumber);
+
         }
     }
 }
---
+++ 8.HW_PrintCurrentDateTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8.HW_PrintCurrentDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

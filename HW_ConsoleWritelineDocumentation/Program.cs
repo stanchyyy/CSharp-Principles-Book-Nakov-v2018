bab439b48b4586f288f569ced6e1580589cf6745
stanchyyy <stanchyyy@gmail.com>
2021-09-22 00:58:52 +0300

    homework 1-6 solved;

---
+++ HW_ConsoleWritelineDocumentation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW_ConsoleWritelineDocumentation
+{
+    class HW_ConsoleWritelineMethodsDocumentation
+    {
+        static void Main(string[] args)
+        {
+            //Task: Find the class explanation of System.Console in the standard .NET API Library.
+
+            //Action: Read https://docs.microsoft.com/en-us/dotnet/api/system.console.writeline?view=net-5.0
+        }
+    }
+}
---
+++ HW_EditHelloCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW_EditHelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task: Modify the HW-HelloCSharp so that the message differs to something like "Good day!".
+            Console.WriteLine("Good day programmer!");
+        }
+    }
+}

6facd286a312b10c16413cd951fb9d941bf54a74
stanchyyy <stanchyyy@gmail.com>
2021-09-22 23:02:01 +0300

    Task 12 solved

---
+++ IntroToProgramming/12.HW_ExplainNetCoreAndStandard/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _12.HW_ExplainNetCoreAndStandard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 12 : Explain the differences between .net and .net Core
+
+            //Solution
+            //1:.NET Core runs on Windows, Linux and macOS.NET Framework runs on Windows.
+            //2:.NET Core does not currently support creating desktop applications with a user interface.
+            //3: .NET Core is fully open source, whereas only a subset of the .NET Framework is open source.
+        }
+    }
+}

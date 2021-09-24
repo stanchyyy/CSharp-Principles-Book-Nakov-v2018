7bf96cf9788b3c4b3c40263e30d9065f38ce9849
stanchyyy <stanchyyy@gmail.com>
2021-09-25 01:06:39 +0300

    Solved task 10; Solved task 11; Solved task 12; Added task 10; Added task 11; Added task 12; Added task 13 Added task 14

---
+++ IntroToProgramking/11.HW_ConsoleWriteLineAngeAndAgeAfter10Years/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _11.HW_ConsoleWriteLineAngeAndAgeAfter10Years
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            //Task 11: Write program that will read from the console your age annd will also print in the console your age after 10 years;
+            int addYears = 10;
+            Console.WriteLine("Enter your current age:");
+            int currentAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Age after 10 years {  currentAge + addYears}");
+        }
+    }
+}

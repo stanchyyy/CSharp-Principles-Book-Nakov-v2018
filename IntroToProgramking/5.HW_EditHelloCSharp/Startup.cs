09992c0460298f1066d0ca164031c26fdf13a36f
stanchyyy <stanchyyy@gmail.com>
2021-09-25 00:57:55 +0300

    Added task 5; Solved task 5;

---
+++ IntroToProgramking/5.HW_EditHelloCSharp/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _5.HW_EditHelloCSharp
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            //Task: Modify the HW-HelloCSharp so that the message differs to something like "Good day!".
+            Console.WriteLine("Good day programmer!");
+        }
+    }
+}

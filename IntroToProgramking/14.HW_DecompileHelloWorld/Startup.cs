5eff83a5255b8aae41eedc036543761c0f37cad2
stanchyyy <stanchyyy@gmail.com>
2021-09-25 01:08:32 +0300

    Added 14 jroject.

---
+++ IntroToProgramking/14.HW_DecompileHelloWorld/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _14.HW_DecompileHelloWorld
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Task 14. Decompiile HelloWorld.
+
+                //Actions:
+                //Install dotnetPeek;
+                //open Intro to programming dll;
+                //Look around the files;
+            }
+        }
+}

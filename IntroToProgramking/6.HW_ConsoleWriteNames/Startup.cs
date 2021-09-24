9cc19bd2a32177679483dddc78a0fecf128c4d26
stanchyyy <stanchyyy@gmail.com>
2021-09-25 00:58:43 +0300

    Added task 6; Solved task 6;

---
+++ IntroToProgramking/6.HW_ConsoleWriteNames/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6.HW_ConsoleWriteNames
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            //Task: 6. Write program that prints your first and last name on the console.
+            //Solution:
+
+            string firstName = "Stanislav";
+            string lastName = "Atanasov";
+
+            Console.WriteLine($"My name is {firstName} {lastName}.");
+        }
+    }
+}

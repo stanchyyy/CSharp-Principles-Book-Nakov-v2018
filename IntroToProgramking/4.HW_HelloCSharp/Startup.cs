85afc449cb285c43a979d9868c724d0439defae4
stanchyyy <stanchyyy@gmail.com>
2021-09-25 00:56:50 +0300

    Added Task 4; Solved task 4;

---
+++ IntroToProgramking/4.HW_HelloCSharp/Startup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4.HW_HelloCSharp
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            //Task - Compile and run the example code in the chapter through the console with Visual Studio.
+            Console.WriteLine("Hello C#");
+        }
+    }
+}

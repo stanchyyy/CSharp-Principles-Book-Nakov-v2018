7f25ccce3435dfb1430196b2c1d7b474dbbff6b7
stanchyyy <stanchyyy@gmail.com>
2021-09-25 00:53:10 +0300

    Added  Task 1; Solved Task 1;

---
+++ IntroToProgramking/1.HW_MicrosoftVisualStudio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1.HW_MicrosoftVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task
+            //Get familiar with the Microsoft Visual Studio and Microsoft Developer Network
+            //(MSDN) Library Documentation.Install Visual Studio.
+
+            //Action : VS 2019 64bit Community edition installed;
+            //Action : https://docs.microsoft.com/en-us/dotnet/csharp/ read;
+        }
+    }
+}

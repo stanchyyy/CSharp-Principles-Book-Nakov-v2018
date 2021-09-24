533032ccb1622155932c9ae7cf0b853e69eed5a2
stanchyyy <stanchyyy@gmail.com>
2021-09-25 01:01:54 +0300

    Added task 9; Solved task 9;

---
+++ IntroToProgramking/8.HW_PrintCurrentDateTime/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _8.HW_PrintCurrentDateTime
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            //Task:8. Write program that prints the cuurerent date and time;
+            DateTime nowDateAndTime = DateTime.Now;
+            Console.WriteLine(nowDateAndTime);
+        }
+    }
+}
---
+++ IntroToProgramking/9.HW_FindSQRT12345/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _9.HW_FindSQRT12345
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            //Task 9: Find SQRT from 12345;
+            double number = 12345;
+            double findSqrt = Math.Sqrt(number);
+            Console.WriteLine(findSqrt);
+        }
+    }
+}
